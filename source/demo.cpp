871fd3e96d33f16a2a8069e1c5396bdd7d717fa4
Popov Evgeniy <tuzik87@inbox.ru>
2022-09-02 19:02:17 +0700

    Source code synced with Eugene game development kit 6.2.6

--- source/demo.cpp
+++ source/demo.cpp
@@ -3,95 +3,123 @@
 int main()
 {
  char perfomance[8];
- GRAYGDK::Timer timer;
- GRAYGDK::Screen screen;
- GRAYGDK::System System;
- GRAYGDK::Keyboard keyboard;
- GRAYGDK::Gamepad gamepad;
- GRAYGDK::Mouse mouse;
- GRAYGDK::Multimedia media;
- GRAYGDK::Image image;
- GRAYGDK::Background space;
- GRAYGDK::Sprite ship,font;
- GRAYGDK::Text text;
- System.enable_logging("log.txt");
+ GRAYGDK::Common::Timer timer;
+ GRAYGDK::Input::Keyboard keyboard;
+ GRAYGDK::Input::Gamepad gamepad;
+ GRAYGDK::Input::Mouse mouse;
+ GRAYGDK::Misc::Multimedia media;
+ GRAYGDK::Graphics::Screen screen;
+ GRAYGDK::Graphics::Image image;
+ GRAYGDK::Graphics::Background space;
+ GRAYGDK::Graphics::Sprite ship;
+ GRAYGDK::Graphics::Text text;
+ GRAYGDK::Tools::enable_logging("log.txt");
  keyboard.initialize();
- space.initialize(screen.get_handle());
- ship.initialize(screen.get_handle());
- font.initialize(screen.get_handle());
+ screen.initialize();
  image.load_tga("space.tga");
- space.load_image(image);
+ space.load_background(image);
+ space.prepare(screen);
  image.load_tga("ship.tga");
- ship.load_sprite(image,HORIZONTAL_STRIP,2);
- ship.set_position(screen.get_frame_width()/2,screen.get_frame_height()/2);
+ ship.load_sprite(image,GRAYGDK::HORIZONTAL_ANIMATED,2);
+ ship.set_position(screen.get_width()/2,screen.get_height()/2);
  image.load_tga("font.tga");
- font.load_image(image);
- text.load_font(font.get_handle());
- text.set_position(font.get_width(),font.get_width());
- screen.clear_screen();
- space.resize_image(screen.get_frame_width(),screen.get_frame_height());
- space.set_kind(NORMAL_BACKGROUND);
+ text.load_font(image);
+ text.set_position(text.get_font_width(),text.get_font_height());
  mouse.hide();
  timer.set_timer(1);
  media.initialize();
  media.load("space.mp3");
  memset(perfomance,0,8);
- screen.initialize();
  while(screen.sync())
  {
   gamepad.update();
-  if (media.check_playing()==false) media.play();
-  if (mouse.check_press(MOUSE_LEFT)==true) break;
-  if (keyboard.check_hold(57)==true) break;
-  if (keyboard.check_hold(72)==true) ship.decrease_y(2);
-  if (keyboard.check_hold(80)==true) ship.increase_y(2);
-  if (keyboard.check_hold(75)==true) ship.decrease_x(2);
-  if (keyboard.check_hold(77)==true) ship.increase_x(2);
-  if (keyboard.check_press(71)==true) ship.horizontal_mirror();
-  if (keyboard.check_press(79)==true) ship.vertical_mirror();
-  if (gamepad.check_hold(GAMEPAD_BUTTON2)==true) break;
-  if (gamepad.check_press(GAMEPAD_BUTTON4)==true) ship.horizontal_mirror();
-  if (gamepad.check_press(GAMEPAD_BUTTON3)==true) ship.vertical_mirror();
+  media.play_loop();
+  if (mouse.check_press(GRAYGDK::MOUSE_LEFT)==true)
+  {
+   break;
+  }
+  if (keyboard.check_hold(57)==true)
+  {
+   break;
+  }
+  if (keyboard.check_hold(72)==true)
+  {
+   ship.decrease_y(2);
+  }
+  if (keyboard.check_hold(80)==true)
+  {
+   ship.increase_y(2);
+  }
+  if (keyboard.check_hold(75)==true)
+  {
+   ship.decrease_x(2);
+  }
+  if (keyboard.check_hold(77)==true)
+  {
+   ship.increase_x(2);
+  }
+  if (gamepad.check_hold(GRAYGDK::GAMEPAD_BUTTON2)==true)
+  {
+   break;
+  }
   switch (gamepad.get_dpad())
   {
-   case GAMEPAD_UP:
-   ship.decrease_y(1);
+   case GRAYGDK::GAMEPAD_UP:
+   ship.decrease_y();
    break;
-   case GAMEPAD_DOWN:
-   ship.increase_y(1);
+   case GRAYGDK::GAMEPAD_DOWN:
+   ship.increase_y();
    break;
-   case GAMEPAD_UPLEFT:
-   ship.decrease_y(1);
-   ship.decrease_x(1);
+   case GRAYGDK::GAMEPAD_UPLEFT:
+   ship.decrease_y();
+   ship.decrease_x();
    break;
-   case GAMEPAD_UPRIGHT:
-   ship.decrease_y(1);
-   ship.increase_x(1);
+   case GRAYGDK::GAMEPAD_UPRIGHT:
+   ship.decrease_y();
+   ship.increase_x();
    break;
-   case GAMEPAD_DOWNLEFT:
-   ship.increase_y(1);
-   ship.decrease_x(1);
+   case GRAYGDK::GAMEPAD_DOWNLEFT:
+   ship.increase_y();
+   ship.decrease_x();
    break;
-   case GAMEPAD_DOWNRIGHT:
-   ship.increase_y(1);
-   ship.increase_x(1);
+   case GRAYGDK::GAMEPAD_DOWNRIGHT:
+   ship.increase_y();
+   ship.increase_x();
    break;
-   case GAMEPAD_LEFT:
-   ship.decrease_x(1);
+   case GRAYGDK::GAMEPAD_LEFT:
+   ship.decrease_x();
    break;
-   case GAMEPAD_RIGHT:
-   ship.increase_x(1);
+   case GRAYGDK::GAMEPAD_RIGHT:
+   ship.increase_x();
    break;
-   case GAMEPAD_NONE:
+   default:
    ;
    break;
   }
-  if (gamepad.get_stick_x(GAMEPAD_LEFT_STICK)==GAMEPAD_NEGATIVE_DIRECTION) ship.decrease_x(1);
-  if (gamepad.get_stick_x(GAMEPAD_LEFT_STICK)==GAMEPAD_POSITIVE_DIRECTION) ship.increase_x(1);
-  if (gamepad.get_stick_y(GAMEPAD_LEFT_STICK)==GAMEPAD_NEGATIVE_DIRECTION) ship.decrease_y(1);
-  if (gamepad.get_stick_y(GAMEPAD_LEFT_STICK)==GAMEPAD_POSITIVE_DIRECTION) ship.increase_y(1);
-  if (ship.get_x()>screen.get_frame_width()) ship.set_x(screen.get_frame_width()/2);
-  if (ship.get_y()>screen.get_frame_height()) ship.set_y(screen.get_frame_height()/2);
+  if (gamepad.get_stick_x(GRAYGDK::GAMEPAD_LEFT_STICK)==GRAYGDK::GAMEPAD_NEGATIVE_DIRECTION)
+  {
+   ship.decrease_x();
+  }
+  if (gamepad.get_stick_x(GRAYGDK::GAMEPAD_LEFT_STICK)==GRAYGDK::GAMEPAD_POSITIVE_DIRECTION)
+  {
+   ship.increase_x();
+  }
+  if (gamepad.get_stick_y(GRAYGDK::GAMEPAD_LEFT_STICK)==GRAYGDK::GAMEPAD_NEGATIVE_DIRECTION)
+  {
+   ship.decrease_y();
+  }
+  if (gamepad.get_stick_y(GRAYGDK::GAMEPAD_LEFT_STICK)==GRAYGDK::GAMEPAD_POSITIVE_DIRECTION)
+  {
+   ship.increase_y();
+  }
+  if (ship.get_x()>screen.get_width())
+  {
+   ship.set_x(screen.get_width()/2);
+  }
+  if (ship.get_y()>screen.get_height())
+  {
+   ship.set_y(screen.get_height()/2);
+  }
   itoa(screen.get_fps(),perfomance,10);
   space.draw_background();
   text.draw_text(perfomance);
