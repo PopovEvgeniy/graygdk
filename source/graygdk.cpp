58ca036a16283dfd04b0c9e4847a3d56660d84c7
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2023-10-25 19:56:19 +0700

    Source code synced with Eugene game development kit 7.6.5

--- source/graygdk.cpp
+++ source/graygdk.cpp
@@ -3372,6 +3372,16 @@ namespace GRAYGDK
 
   }
 
+  int Coordinates::get_viewport_width() const
+  {
+   return 2*half_viewport_width;
+  }
+
+  int Coordinates::get_viewport_height() const
+  {
+   return 2*half_viewport_height;
+  }
+
   int Coordinates::get_lowest_cartesian_x() const
   {
    return -1*half_viewport_width;
