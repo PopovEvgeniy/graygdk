8ed024a9ebc55650b5a972b7bbd0126006ee0b6f
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2024-01-28 23:09:35 +0300

    Source code synced with Eugene game development kit 8.0.3

--- source/graygdk.cpp
+++ source/graygdk.cpp
@@ -711,30 +711,6 @@ namespace GRAYGDK
 
   }
 
-  void Resizer::load_image(const unsigned int *target)
-  {
-   size_t index;
-   image[0]=target[0];
-   for (index=image.get_length()-1;index>0;--index)
-   {
-    image[index]=target[index];
-   }
-
-  }
-
-  void Resizer::resize_image(const unsigned int *target)
-  {
-   if ((source_width==target_width) && (source_height==target_height))
-   {
-    this->load_image(target);
-   }
-   else
-   {
-    this->scale_image(target);
-   }
-
-  }
-
   void Resizer::set_setting(const unsigned int width,const unsigned int height)
   {
    source_width=width;
@@ -781,14 +757,9 @@ namespace GRAYGDK
    image.create_buffer();
   }
 
-  void Resizer::make_texture(const unsigned int *target,const unsigned int width,const unsigned int height,const unsigned int limit)
+  bool Resizer::is_dont_need_resize() const
   {
-   this->set_setting(width,height);
-   this->calculate_size();
-   this->correct_size(limit);
-   this->calculate_ratio();
-   this->create_texture();
-   this->resize_image(target);
+   return (source_width==target_width) && (source_height==target_height);
   }
 
   unsigned int Resizer::get_width() const
@@ -804,6 +775,20 @@ namespace GRAYGDK
   unsigned int *Resizer::get_buffer()
   {
    return image.get_buffer();
+  }
+
+  void Resizer::make_texture(const unsigned int *target,const unsigned int width,const unsigned int height,const unsigned int limit)
+  {
+   this->set_setting(width,height);
+   this->calculate_size();
+   this->correct_size(limit);
+   if (this->is_dont_need_resize()==false)
+   {
+    this->calculate_ratio();
+    this->create_texture();
+    this->scale_image(target);
+   }
+
   }
 
   FPS::FPS()
@@ -980,10 +965,8 @@ namespace GRAYGDK
 
   }
 
-  void Rectangle::create_texture(const unsigned int *buffer)
+  void Rectangle::load_texture(const unsigned int width,const unsigned int height,const unsigned int *buffer)
   {
-   Resizer resizer;
-   resizer.make_texture(buffer,this->get_total_width(),this->get_total_height(),MAXIMUM_TEXTURE_SIZE);
    glPixelStorei(GL_UNPACK_ALIGNMENT,1);
    glGenTextures(1,&texture);
    glBindTexture(GL_TEXTURE_2D,texture);
@@ -992,7 +975,22 @@ namespace GRAYGDK
    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_CLAMP);
    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_CLAMP);
-   glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,resizer.get_width(),resizer.get_height(),0,GL_BGRA_EXT,GL_UNSIGNED_BYTE,resizer.get_buffer());
+   glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,width,height,0,GL_BGRA_EXT,GL_UNSIGNED_BYTE,buffer);
+  }
+
+  void Rectangle::create_texture(const unsigned int *buffer)
+  {
+   Resizer resizer;
+   resizer.make_texture(buffer,this->get_total_width(),this->get_total_height(),MAXIMUM_TEXTURE_SIZE);
+   if (resizer.is_dont_need_resize()==true)
+   {
+    this->load_texture(this->get_total_width(),this->get_total_height(),buffer);
+   }
+   else
+   {
+    this->load_texture(resizer.get_width(),resizer.get_height(),resizer.get_buffer());
+   }
+
   }
 
   void Rectangle::delete_texture()
