382a5fe74f0280fcd0562a838d549ff47eeb8bbc
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-02-17 21:44:20 +0700

    The source code synced with the Eugene game development kit 9.5.4

--- source/graygdk.cpp
+++ source/graygdk.cpp
@@ -3584,17 +3584,12 @@ namespace GRAYGDK
    v_offset+=v_speed;
   }
 
-  void Parallax::reset_u_offset()
+  void Parallax::reset_offset()
   {
    if (u_offset>1.0f)
    {
     u_offset=0.0f;
    }
-
-  }
-
-  void Parallax::reset_v_offset()
-  {
    if (v_offset>1.0f)
    {
     v_offset=0.0f;
@@ -3700,8 +3695,7 @@ namespace GRAYGDK
   void Parallax::draw()
   {
    this->calculate_offset();
-   this->reset_u_offset();
-   this->reset_v_offset();
+   this->reset_offset();
    this->set_texture_coordinates();
    stage.draw(false);
   }
