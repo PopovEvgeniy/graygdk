d19fc2f7f40fd815004f51de7fcaf55370c99c36
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-05-24 20:39:47 +0700

    The source code synced with the Black game development kit 7.3.9

--- source/graygdk.cpp
+++ source/graygdk.cpp
@@ -2268,19 +2268,9 @@ namespace GRAYGDK
    highest_y_offset=((screen_height-viewport_height)*screen_height)/viewport_height;
   }
 
-  bool Camera::check_viewport_width(const unsigned int width) const
-  {
-   return (width>0) && (width<=screen_width);
-  }
-
-  bool Camera::check_viewport_heigth(const unsigned int height) const
-  {
-   return (height>0) && (height<=screen_height);
-  }
-
   void Camera::set_viewport_width(const unsigned int width)
   {
-   if (this->check_viewport_width(width)==true)
+   if ((width>0) && (width<=screen_width))
    {
     viewport_width=width;
    }
@@ -2293,7 +2283,7 @@ namespace GRAYGDK
 
   void Camera::set_viewport_heigth(const unsigned int height)
   {
-   if (this->check_viewport_heigth(height)==true)
+   if ((height>0) && (height<=screen_height))
    {
     viewport_height=height;
    }
