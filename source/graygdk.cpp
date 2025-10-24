0198d646235260f5f8c9c8a84e9db6f0da0b23c4
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-10-24 23:53:32 +0700

    The source code synced with the Eugene Game Development Kit 9.7.8

--- source/graygdk.cpp
+++ source/graygdk.cpp
@@ -3663,9 +3663,9 @@ namespace GRAYGDK
    stage.complex_mirror();
   }
 
-  void Background::set_target(const unsigned int target)
+  unsigned int Background::set_target(const unsigned int target)
   {
-   stage.set_target(target);
+   return stage.set_target(target);
   }
 
   void Background::step()
