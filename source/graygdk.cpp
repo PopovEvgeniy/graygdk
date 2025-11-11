925d1179944079e3922ac537863defde4f0a7c47
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-11-11 21:51:11 +0700

    The source code synced with the Eugene Game Development Kit 9.9

--- source/graygdk.cpp
+++ source/graygdk.cpp
@@ -3858,6 +3858,11 @@ namespace GRAYGDK
    return stage.is_complex_mirrored();
   }
 
+  bool Scene::is_mirrored() const
+  {
+   return stage.is_mirrored();
+  }
+
   void Scene::draw()
   {
    stage.draw(false);
